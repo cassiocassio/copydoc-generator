a19e6017e0c6869835c372a35fcc4184f5b34ebd
Martin Storey <martin@cassiocassio.co.uk>
2019-02-28 17:59:38 +0100

    again

--- src/app/markup/Key_features_ticklist_large_orange.js
+++ src/app/markup/Key_features_ticklist_large_orange.js
@@ -1,22 +1,39 @@
 import React from "react";
 
 export default (
-
-<section class="p-strip u-no-padding--top is-bordered">
-  <div class="row">
-    <div class="col-12">
-      <ul class="p-list is-split">
-        <li class="p-list__item is-ticked">Save 50% with fully managed OpenStack compared to VMWare</li>
-        <li class="p-list__item is-ticked">Cloud infrastructure provides developer self-service agility</li>
-        <li class="p-list__item is-ticked">Architecture flexibility supports wide range of sector needs</li>
-        <li class="p-list__item is-ticked">Ubuntu is the infrastructure leader for large-scale Linux operations</li>
-        <li class="p-list__item is-ticked">Workload analysis, migration plan and automation</li>
-        <li class="p-list__item is-ticked">GPGPU and FPGA pass-through for workload acceleration</li>
-        <li class="p-list__item is-ticked">Integrated Kubernetes offering for modern application ops</li>
-        <li class="p-list__item is-ticked">Optimised KVM for high performance and security</li>
-        <li class="p-list__item is-ticked">Scale from one to hundreds of racks</li>
-      </ul>
+  <section class="p-strip u-no-padding--top is-bordered">
+    <div class="row">
+      <div class="col-12">
+        <ul class="p-list is-split">
+          <li class="p-list__item is-ticked">
+            Save 50% with fully managed OpenStack compared to VMWare
+          </li>
+          <li class="p-list__item is-ticked">
+            Cloud infrastructure provides developer self-service agility
+          </li>
+          <li class="p-list__item is-ticked">
+            Architecture flexibility supports wide range of sector needs
+          </li>
+          <li class="p-list__item is-ticked">
+            Ubuntu is the infrastructure leader for large-scale Linux operations
+          </li>
+          <li class="p-list__item is-ticked">
+            Workload analysis, migration plan and automation
+          </li>
+          <li class="p-list__item is-ticked">
+            GPGPU and FPGA pass-through for workload acceleration
+          </li>
+          <li class="p-list__item is-ticked">
+            Integrated Kubernetes offering for modern application ops
+          </li>
+          <li class="p-list__item is-ticked">
+            Optimised KVM for high performance and security
+          </li>
+          <li class="p-list__item is-ticked">
+            Scale from one to hundreds of racks
+          </li>
+        </ul>
+      </div>
     </div>
-  </div>
-</section>
+  </section>
 );
--- src/app/markup/Tail_micronav.js
+++ src/app/markup/Tail_micronav.js
@@ -1,57 +1,96 @@
 import React from "react";
 
 export default (
-
-<div class="row">
-        <div class="col-4">
-            <h3 class="p-footer__title p-heading--four">MAAS</h3>
-            <ul class="p-footer__list p-list">
-                <li class="p-list__item">
-                    <a class="p-link--strong" href="/">Home</a>
-                </li>
-                <li class="p-list__item">
-                    <a class="p-link--strong" href="/install">Install MAAS</a>
-                </li>
-                <li class="p-list__item">
-                    <a class="p-link--strong" href="/how-it-works">How it works</a>
-                </li>
-                <li class="p-list__item">
-                    <a class="p-link--external p-link--strong" href="https://docs.maas.io">Docs</a>
-                </li>
-                <li class="p-list__item">
-                    <a class="p-link--strong" href="/tour">Tour</a>
-                </li>
-            </ul>
-        </div>
-
-        <div class="col-4">
-            <h3 class="p-footer__title p-heading--four">Need help?</h3>
-            <ul class="p-footer__list p-list">
-                <li class="p-list__item">
-                    <a class="p-link--external p-link--strong" href="https://askubuntu.com/questions/tagged/maas">Ask Ubuntu</a>
-                </li>
-                <li class="p-list__item">
-                    <a class="p-link--external p-link--strong" href="http://webchat.freenode.net/?channels=maas">Chat with MAAS on freenode</a>
-                </li>
-                <li class="p-list__item">
-                    <a class="p-link--external p-link--strong" href="https://lists.ubuntu.com/mailman/listinfo/maas-devel">Join the mailing list</a>
-                </li>
-                <li class="p-list__item">
-                    <a class="p-link--strong" href="/contact-us">Commercial support</a>
-                </li>
-            </ul>
-        </div>
-
-        <div class="col-4">
-            <h3 class="p-footer__title p-heading--four">Contribute</h3>
-            <ul class="p-footer__list p-list">
-                <li class="p-list__item">
-                    <a class="p-link--external p-link--strong" href="https://launchpad.net/maas">Launchpad</a>
-                </li>
-                <li class="p-list__item">
-                    <a class="p-link--external p-link--strong" href="https://docs.maas.io/en/contributing-writing">Contribute to documentation</a>
-                </li>
-            </ul>
-        </div>
+  <div class="row">
+    <div class="col-4">
+      <h3 class="p-footer__title p-heading--four">MAAS</h3>
+      <ul class="p-footer__list p-list">
+        <li class="p-list__item">
+          <a class="p-link--strong" href="/">
+            Home
+          </a>
+        </li>
+        <li class="p-list__item">
+          <a class="p-link--strong" href="/install">
+            Install MAAS
+          </a>
+        </li>
+        <li class="p-list__item">
+          <a class="p-link--strong" href="/how-it-works">
+            How it works
+          </a>
+        </li>
+        <li class="p-list__item">
+          <a
+            class="p-link--external p-link--strong"
+            href="https://docs.maas.io"
+          >
+            Docs
+          </a>
+        </li>
+        <li class="p-list__item">
+          <a class="p-link--strong" href="/tour">
+            Tour
+          </a>
+        </li>
+      </ul>
     </div>
-    );
+
+    <div class="col-4">
+      <h3 class="p-footer__title p-heading--four">Need help?</h3>
+      <ul class="p-footer__list p-list">
+        <li class="p-list__item">
+          <a
+            class="p-link--external p-link--strong"
+            href="https://askubuntu.com/questions/tagged/maas"
+          >
+            Ask Ubuntu
+          </a>
+        </li>
+        <li class="p-list__item">
+          <a
+            class="p-link--external p-link--strong"
+            href="http://webchat.freenode.net/?channels=maas"
+          >
+            Chat with MAAS on freenode
+          </a>
+        </li>
+        <li class="p-list__item">
+          <a
+            class="p-link--external p-link--strong"
+            href="https://lists.ubuntu.com/mailman/listinfo/maas-devel"
+          >
+            Join the mailing list
+          </a>
+        </li>
+        <li class="p-list__item">
+          <a class="p-link--strong" href="/contact-us">
+            Commercial support
+          </a>
+        </li>
+      </ul>
+    </div>
+
+    <div class="col-4">
+      <h3 class="p-footer__title p-heading--four">Contribute</h3>
+      <ul class="p-footer__list p-list">
+        <li class="p-list__item">
+          <a
+            class="p-link--external p-link--strong"
+            href="https://launchpad.net/maas"
+          >
+            Launchpad
+          </a>
+        </li>
+        <li class="p-list__item">
+          <a
+            class="p-link--external p-link--strong"
+            href="https://docs.maas.io/en/contributing-writing"
+          >
+            Contribute to documentation
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+);
